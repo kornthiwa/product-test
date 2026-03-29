7ce44b35a94ef3602186345598e9bc69d667e10c
Ford-Kornthiwa <kornthiwa@gmail.com>
2026-03-29 17:06:47 +0700

    fix env

--- app/lib/api/client.ts
+++ app/lib/api/client.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL = (
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001"
-).replace(/\/$/, "");
+const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
 
 export const api = axios.create({
   baseURL,
